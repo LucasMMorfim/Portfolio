e231051dc2b080949afb797275cee5ac371fc5e8
Lucas Morfim <106320326+LucasMMorfim@users.noreply.github.com>
2024-10-21 14:51:33 -0300

    create header

---
+++ components/Social.jsx
@@ -0,0 +1,16 @@
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icons: <FaGithub />, path: "" },
+  { icons: <FaLinkedinIn />, path: "" },
+  { icons: <FaYoutube />, path: "" },
+  { icons: <FaTwitter />, path: "" },
+]
+
+const Social = () => {
+  return (
+    <div>Social</div>
+  )
+}
+
+export default Social
